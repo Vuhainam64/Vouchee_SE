be700da01b21715de71f8859407e80f5be864b0a
PhatBaka <nguyenquyphat2711@gmail.com>
2024-10-05 13:19:06 +0700

    UPDATE: Remove folder

---
+++ Vouchee.Business/Services/IVoucheeBackgroundService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vouchee.Business.Services
+{
+    public interface IVoucheeBackgroundService
+    {
+        // CREATE
+
+        // UPDATE
+        public Task UpdatePromotionStatus();
+
+        // DELETE
+    }
+}
