d2df8458849bcec85b3e1c8aa7b60b74de09a4e4
caothang7a7 <caothang7a7@gmail.com>
2024-10-16 21:24:31 +0700

    add Cart in DB

--- Vouchee.Data/Models/Entities/Cart.cs
+++ Vouchee.Data/Models/Entities/Cart.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Vouchee.Data.Models.Entities
 {
-    internal class Cart
+    [Table("Cart")]
+    [Index(nameof(UserId), Name = "IX_Cart_UserId")]
+    public partial class Cart
     {
+        public Cart()
+        {
+            Vouchers = new HashSet<Voucher>();
+        }
+        [InverseProperty(nameof(Voucher.Carts))]
+        public virtual ICollection<Voucher> Vouchers { get; set; }
+        public Guid? UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("Cart")]
+        public virtual User? User { get; set; }
+        
+        public decimal? Quantity { get; set; }
     }
 }
