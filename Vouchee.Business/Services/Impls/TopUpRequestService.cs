133a32913fb78737be54a6861599c4a939de3c40
PhatBaka <nguyenquyphat2711@gmail.com>
2024-11-10 21:52:22 +0700

    FIX: create top up request

--- Vouchee.Business/Services/Impls/TopUpRequestService.cs
+++ Vouchee.Business/Services/Impls/TopUpRequestService.cs
@@ -37,34 +37,31 @@ namespace Vouchee.Business.Services.Impls
 
         public async Task<ResponseMessage<Guid>> CreateTopUpRequest(CreateTopUpRequestDTO createTopUpRequestDTO, ThisUserObj thisUserObj)
         {
-            return null;
+            var user = await _userRepository.GetByIdAsync(thisUserObj.userId, includeProperties: x => x.Include(x => x.BuyerWallet), isTracking: true);
 
-            //var user = await _userRepository.GetByIdAsync(thisUserObj.userId, includeProperties: x => x.Include(x => x.Wallets), isTracking: true);
-            //var buyerWallet = user.Wallets.FirstOrDefault(x => x.Type == WalletTypeEnum.BUYER.ToString());
-            
-            //if (buyerWallet == null)
-            //{
-            //    throw new Exception("User này chưa có ví người mua");
-            //}
+            if (user.BuyerWallet == null)
+            {
+                throw new Exception("User này chưa có ví người mua");
+            }
 
-            //TopUpRequest topUpRequest = _mapper.Map<TopUpRequest>(createTopUpRequestDTO);
-            //topUpRequest.CreateBy = thisUserObj.userId;
-            //topUpRequest.WalletTransaction = new()
-            //{
-            //    CreateBy = thisUserObj.userId,
-            //    CreateDate = DateTime.Now,
-            //    Status = WalletTransactionStatusEnum.PENDING.ToString(),
-            //    Amount = topUpRequest.Amount,
-            //    BuyerWalletId = buyerWallet.Id
-            //};
+            TopUpRequest topUpRequest = _mapper.Map<TopUpRequest>(createTopUpRequestDTO);
+            topUpRequest.CreateBy = thisUserObj.userId;
+            topUpRequest.WalletTransaction = new()
+            {
+                CreateBy = thisUserObj.userId,
+                CreateDate = DateTime.Now,
+                Status = WalletTransactionStatusEnum.PENDING.ToString(),
+                Amount = topUpRequest.Amount,
+                BuyerWalletId = user.BuyerWallet.Id
+            };
 
-            //var result = await _topUpRequestRepository.AddAsync(topUpRequest);
-            //return new ResponseMessage<Guid>()
-            //{
-            //    message = "Tạo top up request thành công",
-            //    result = true,
-            //    value = (Guid) result
-            //};
+            var result = await _topUpRequestRepository.AddAsync(topUpRequest);
+            return new ResponseMessage<Guid>()
+            {
+                message = "Tạo top up request thành công",
+                result = true,
+                value = (Guid)result
+            };
         }
 
         public async Task<GetTopUpRequestDTO> GetTopUpRequestById(Guid id)
