6f1f294ca6d0bcd4312301d616fe13af1c3488fc
PhatBaka <nguyenquyphat2711@gmail.com>
2024-11-22 23:55:56 +0700

    CREATE: noti

---
+++ Vouchee.Data/Migrations/20241122165430_AddToken.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vouchee.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Notification");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankNumber",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DeviceToken",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Platform = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceToken_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceToken_UserId",
+                table: "DeviceToken",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeviceToken");
+
+            migrationBuilder.DropColumn(
+                name: "BankName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "BankNumber",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Notification",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
