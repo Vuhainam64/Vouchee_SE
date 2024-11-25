1214019b38c6097637395287a71f0db4896692f6
PhatBaka <nguyenquyphat2711@gmail.com>
2024-11-25 20:40:08 +0700

    ADD: logout

--- Vouchee.API/Controllers/AuthController.cs
+++ Vouchee.API/Controllers/AuthController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Google.Api.Gax;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Vouchee.API.Helpers;
+using Vouchee.Business.Models;
 using Vouchee.Business.Models.DTOs;
 using Vouchee.Business.Models.ViewModels;
 using Vouchee.Business.Services;
 using Vouchee.Data.Models.Constants.Enum.Status;
+using Vouchee.Data.Models.Entities;
 
 namespace Vouchee.API.Controllers
 {
@@ -15,10 +19,16 @@ namespace Vouchee.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly IUserService _userService;
+        private readonly IDeviceTokenService _deviceTokenService;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService,
+                              IUserService userService,
+                              IDeviceTokenService deviceTokenService)
         {
             _authService = authService;
+            _userService = userService;
+            _deviceTokenService = deviceTokenService;
         }
 
         [HttpPost("login_with_google_token")]
@@ -33,6 +43,16 @@ namespace Vouchee.API.Controllers
             }
 
             var result = await _authService.GetToken(token, platform, deviceToken);
+            return Ok(result);
+        }
+
+        [HttpDelete("logout")]
+        public async Task<IActionResult> Logout([FromQuery] string token,
+                                                    [FromQuery] DevicePlatformEnum platform = 0)
+        {
+            ThisUserObj currentUser = await GetCurrentUserInfo.GetThisUserInfo(HttpContext, _userService);
+
+            var result = await _deviceTokenService.RemoveDeviceTokenAsync(currentUser.userId, token, platform);
             return Ok(result);
         }
     }
