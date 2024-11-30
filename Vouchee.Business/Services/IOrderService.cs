bcbab1260603ec512f9c84b0ffc2f2ba7f2fdf3c
PhatBaka <nguyenquyphat2711@gmail.com>
2024-12-01 04:19:26 +0700

    ADD: seller order detail

--- Vouchee.Business/Services/IOrderService.cs
+++ Vouchee.Business/Services/IOrderService.cs
@@ -10,23 +10,12 @@ namespace Vouchee.Business.Services
 {
     public interface IOrderService
     {
-        // CREATE
-        // public Task<Guid?> CreateOrderAsync(CreateOrderDTO createOrderDTO, ThisUserObj thisUserObj);
         public Task<ResponseMessage<string>> CreateOrderAsync(ThisUserObj thisUserObj, CheckOutViewModel checkOutViewModel);
 
         // READ
         public Task<GetDetailOrderDTO> GetOrderByIdAsync(string id);
         public Task<DynamicResponseModel<GetOrderDTO>> GetOrdersAsync(PagingRequest pagingRequest, OrderFilter orderFilter, ThisUserObj? thisUserObj);
         public Task<DynamicResponseModel<GetOrderDTO>> GetSellerOrderAsync(PagingRequest pagingRequest, OrderFilter orderFilter, ThisUserObj thisUserObj);
-        public Task<GetDetailOrderDTO> GetDetailSellerOrderAsync(string orderId, ThisUserObj thisUserObj);
-
-        // UPDATE
-        //public Task<bool> UpdateOrderAsync(string id, UpdateOrderDTO updateOrderDTO, ThisUserObj thisUserObj);
-        //public Task<ResponseMessage<bool>> UpdateOrderTransactionAsync(string id, Guid orderTransactionId, ThisUserObj thisUserObj);
-        //public Task<bool> AssignCodeToOrderAsync(string orderId, Guid modalId, VoucherCodeList voucherCodeId);
-        //public Task<bool> UpdateUserPointAsync(Guid orderId);
-
-        // DELETE
-        //public Task<bool> DeleteOrderAsync(string id);
+        public Task<GetDetailSellerOrderDTO> GetDetailSellerOrderAsync(string orderId, ThisUserObj thisUserObj);
     }
 }
