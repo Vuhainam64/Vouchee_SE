a24a5d953c0685d63101298a0136fa5b0e62c439
PhatBaka <nguyenquyphat2711@gmail.com>
2024-12-18 12:00:00 +0700

    Add: voucher code

---
+++ Vouchee.Data/Migrations/20241218045844_AddNewCodeInVoucherCode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vouchee.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewCodeInVoucherCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NewCode",
+                table: "VoucherCode",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NewCode",
+                table: "VoucherCode");
+        }
+    }
+}
