c766f3c3f184d91a8e75a3996ddee854d3050cb1
caothang7a7 <caothang7a7@gmail.com>
2024-12-18 17:59:36 +0700

    add entity for api create user

--- Vouchee.Data/Models/DTOs/UserDTO.cs
+++ Vouchee.Data/Models/DTOs/UserDTO.cs
@@ -19,6 +19,8 @@ namespace Vouchee.Business.Models.DTOs
     {
         [Required(ErrorMessage = "Email là cần thiết")]
         public string? email { get; set; }
+        [Required(ErrorMessage = "name là cần thiết")]
+        public string? Name { get; set; }
         [Required(ErrorMessage = "Mật khẩu là cần thiết")]
         public string? hashPassword { get; set; }
         public DateTime? createDate = DateTime.Now;
