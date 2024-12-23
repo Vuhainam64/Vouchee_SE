12a117518e0dae3f20e58abe8ed021305a322056
PhatBaka <nguyenquyphat2711@gmail.com>
2024-12-23 17:58:26 +0700

    Add voucher code comment

---
+++ Vouchee.Data/Migrations/20241223105742_AddVoucherCodeComment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vouchee.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVoucherCodeComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "VoucherCode",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "VoucherCode");
+        }
+    }
+}
