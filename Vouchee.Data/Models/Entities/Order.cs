a217ff6823d7ba2fdea6ffecbe78ba7c25e3985c
PhatBaka <nguyenquyphat2711@gmail.com>
2025-01-17 15:42:47 +0700

    FIX vpointup

--- Vouchee.Data/Models/Entities/Order.cs
+++ Vouchee.Data/Models/Entities/Order.cs
@@ -52,7 +52,10 @@ namespace Vouchee.Data.Models.Entities
         public int UsedBalance { get; set; }
         public int FinalPrice => OrderDetails.Sum(x => x.FinalPrice) - UsedVPoint - UsedBalance;
         public string? GiftEmail { get; set; }
-        public int VPointUp => (int)Math.Ceiling((decimal)(TotalPrice - DiscountPrice - UsedVPoint ) / 1000);
+        public int VPointUp =>
+            ((TotalPrice - DiscountPrice - UsedVPoint) % 1000 < 500)
+                ? (TotalPrice - DiscountPrice - UsedVPoint) / 1000
+                : (TotalPrice - DiscountPrice - UsedVPoint) / 1000 + 1;
         public string? Note { get; set; }
 
         public required string Status { get; set; }
